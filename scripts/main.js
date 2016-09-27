c0258da9460ce80b8346bd6bad54adf95c218b14
tbredin <tom@studiothick.com>
2016-09-27 16:35:16 +1000

    Update 2016-09-27T06:35:05.290Z

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){$(document).foundation(),$("[data-toggle=offCanvasLeft").click(function(n){n.preventDefault()})});
+$(document).ready(function(){$(document).foundation()});
